fda4c3bd5555167411feab41e0d7ade3afac5275
DESKTOP-PNDJBNO\mix83 <https://github.com/Yookdong/v14.git>
2023-07-10 00:47:39 +0900

    초기 커밋.

---
+++ TetrisGame/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "TetrisHelper.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
---
+++ TetrisGame/TetrisGame.cpp
@@ -0,0 +1,14 @@
+﻿#include "Header.h"
+
+int main()
+{
+	float prev = (float)clock() / CLOCKS_PER_SEC;
+	// clock() 프로그램 시작부터 함수가 실행된 시점의 시간을 구한다. CLOCKS_PER_SEC 는 보통 1000 이다.
+
+	while (true)
+	{
+		float currenttime = (float)clock() / CLOCKS_PER_SEC;
+		// prev 에서 clock 함수가 실행된 시점과 currentrime 에서 실행된 시점을 빼주면 while 문이 한 번 돌아간 시간이 계산이 되는 것이다.
+		float deltatime = currenttime - prev;	
+	}
+}
